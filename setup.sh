bb9001499945fa82c3dc117d3a453ad2951e3a7e
miyasumas <miyasumas@users.noreply.github.com>
2022-07-04 22:56:43 +0900

    Move bash files

--- setup.sh
+++ setup.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 BASEDIR=`pwd`
-BACKUP_DIR=$BASEDIR/.backup/`date +'%Y%m%d%H%M%S'`
-DOTFILES=("bashrc" "bash_profile" "profile")
-
-for f in ${DOTFILES[@]}; do
-	if [ ! -L ~/.$f ]; then
-		if [ ! -d $BACKUP_DIR ]; then
-			mkdir -p $BACKUP_DIR
-		fi
-		mv ~/.$f $BACKUP_DIR
-	else
-		rm -f ~/.$f
-	fi
-	ln -sv $BASEDIR/$f ~/.$f
-done
-
 XDG_CONFIG_HOME=~/.config
 mkdir -p $XDG_CONFIG_HOME
+
+# XDG Base Directory Specification
 ln -svf "$BASEDIR/config/"* $XDG_CONFIG_HOME
+
+# vim
 ln -svfn "$BASEDIR/config/vim" ~/.vim
+
+# bash
+ln -svf "$BASEDIR/config/bash/.profile" ~/.profile
+ln -svf "$BASEDIR/config/bash/.bash_profile" ~/.bash_profile
+ln -svf "$BASEDIR/config/bash/.bashrc" ~/.bashrc
 
