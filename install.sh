092d1490bf3d0fdbe2dfefb91d9802ccd0edda87
miyasumas <miyasumas@users.noreply.github.com>
2022-09-21 10:05:50 +0900

    Update intstall.sh

--- install.sh
+++ install.sh
@@ -18,31 +18,30 @@ function create_symlinks() {
   XDG_CONFIG_HOME=$HOME/.config
   mkdir -p $XDG_CONFIG_HOME
 
-  # git
-  ln -svf "$BASEDIR/config/git" $XDG_CONFIG_HOME
-
-  # readline
-  ln -svf "$BASEDIR/config/readline" $XDG_CONFIG_HOME
-
-  # tmux
-  ln -svf "$BASEDIR/config/tmux" $XDG_CONFIG_HOME
-
-  # karabiner
-  KARABINER_CONFIG_DIR=$XDG_CONFIG_HOME/karabiner
-  mkdir -p $KARABINER_CONFIG_DIR
-  ln -svf "$BASEDIR/config/karabiner/"* $KARABINER_CONFIG_DIR
+  # XDG Config
+  TARGET_DIRS=(git readline tmux karabiner)
+  for d in ${TARGET_DIRS[@]}; do
+	TARGET_DIR=$BASEDIR/config/$d
+	if [ ! -d $TARGET_DIR ]; then
+	  echo "Not found: $TARGET_DIR"
+	  continue
+	fi
+	CONFIG_DIR=$XDG_CONFIG_HOME/$d
+	mkdir -p $CONFIG_DIR
+	ln -svfn "$TARGET_DIR"/* $CONFIG_DIR
+  done
 
   # vim
   ln -svfn "$BASEDIR/config/vim" $HOME/.vim
 
   # bash
-  ln -svf "$BASEDIR/config/bash/.profile" $HOME/.profile
-  ln -svf "$BASEDIR/config/bash/.bash_profile" $HOME/.bash_profile
-  ln -svf "$BASEDIR/config/bash/.bashrc" $HOME/.bashrc
+  ln -svfn "$BASEDIR/config/bash/.profile" $HOME/.profile
+  ln -svfn "$BASEDIR/config/bash/.bash_profile" $HOME/.bash_profile
+  ln -svfn "$BASEDIR/config/bash/.bashrc" $HOME/.bashrc
 
   # zsh
-  ln -svf "$BASEDIR/config/zsh/.zshenv" $HOME/.zshenv
-  ln -svf "$BASEDIR/config/zsh/.zshrc" $HOME/.zshrc
+  ln -svfn "$BASEDIR/config/zsh/.zshenv" $HOME/.zshenv
+  ln -svfn "$BASEDIR/config/zsh/.zshrc" $HOME/.zshrc
 }
 
 # Move work directory
